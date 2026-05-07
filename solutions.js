5bddaf8d74db4f9c1342aa2f212d000b3455832c
alhaj-cmd <alhazsarkar5523@gmail.com>
2026-05-07 20:17:46 +0600

    problem solved

--- solutions.js
+++ solutions.js
@@ -1,11 +1,9 @@
+"use strict";
 // problem 1
 /* Create a TypeScript function filterEvenNumbers that accepts an array of numbers and returns a new array containing only the even numbers.*/
-
-function filterEvenNumbers(numbers: number[]): number[] {
+function filterEvenNumbers(numbers) {
     return numbers.filter((num) => num % 2 === 0);
 }
-
-// Even Numbers usage
+// Example usage
 const result = filterEvenNumbers([1, 2, 3, 4, 5, 6, 7, 8]);
-
 console.log(result);
