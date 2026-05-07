e7cb6be4c0b2d32244f89cbacab2ab9426aa2e63
alhaj-cmd <alhazsarkar5523@gmail.com>
2026-05-07 21:25:43 +0600

    problem solving

--- solutions.ts
+++ solutions.ts
@@ -1,11 +1,19 @@
-// problem 1
-/* Create a TypeScript function filterEvenNumbers that accepts an array of numbers and returns a new array containing only the even numbers.*/
+/*  ---- problem 1: Create a TypeScript function filterEvenNumbers that accepts an array of numbers and returns a new array containing only the even numbers.*/
 
 function filterEvenNumbers(numbers: number[]): number[] {
     return numbers.filter((num) => num % 2 === 0);
 }
-
 // Even Numbers usage
 const result = filterEvenNumbers([1, 2, 3, 4, 5, 6, 7, 8]);
-
 console.log(result);
+
+
+/* -------- problem 2 : Write a function reverseString that takes a string as input and returns the reversed version of that string. ----- */
+
+
+function reverseString(text: string) {
+    return text.split("").reverse().join("");
+}
+const resultOne = reverseString('typescript');
+console.log(resultOne);
+
